8cc8dd8cd8b832980ff70f53930d94302c979642
Chanwit Kaewkasi <chanwit@gmail.com>
2020-01-07 11:21:42 +0000

    set hostname scripts

---
+++ set_hostname.sh
@@ -0,0 +1,4 @@
+sudo ~/.wks/bin/ignite -i /home/chanwit/projects/firekube-profile-demo/cluster-key exec firekube-node0 hostname node0
+sudo ~/.wks/bin/ignite -i /home/chanwit/projects/firekube-profile-demo/cluster-key exec firekube-node1 hostname node1
+sudo ~/.wks/bin/ignite -i /home/chanwit/projects/firekube-profile-demo/cluster-key exec firekube-node2 hostname node2
+sudo ~/.wks/bin/ignite -i /home/chanwit/projects/firekube-profile-demo/cluster-key exec firekube-node3 hostname node3
